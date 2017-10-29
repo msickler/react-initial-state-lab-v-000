7c020a35bc14508f27f37517dbb8a912a5439186
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-29 20:42:56 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -8,7 +8,7 @@ export default class Bomb extends React.Component {
 
   render() {
     return (
-      <p>{this.state.secondsLeft} seconds left before I go boom!</p>
+      <p>{this.state.secondsLeft ? `${this.state.secondsLeft} seconds left before I go boom!` : "Boom!"}</p>
     )
   }
 }
